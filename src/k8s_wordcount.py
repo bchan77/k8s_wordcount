45ddbe4969f165e917c5f309192752d91096c93b
Billy <billy@datarobot.com>
2020-07-07 21:47:57 -0400

    Adding some perm. It does nothing yet

--- src/k8s_wordcount.py
+++ src/k8s_wordcount.py
@@ -1,7 +1,35 @@
-import kubernetes
+import logging
+from kubernetes import client, config
+import getopt, sys
+
+
+
+def create_pvc_if_not_exist():
+    print("create_pvc_if_not_exist")
+
+
+def print_usage():
+    print(sys.argv[0] +  "-h " \
+                         "--LOG <DEBUG|INFO|WARNING|ERROR|CRITICAL>"
+          )
+
 
 def main():
-    print("Hello wordcount")
+    LOGGING=logging.INFO # By default, just log info
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "", ["--log="])
+
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(-1)
+
+    for opt,arg in opts:
+        if opt.upper() == ("--LOG"):
+            if(arg.upper() not in ['DEBUG', "INFO", "WARNING", "ERROR","CRITICAL"]):
+                print("ERROR: --Log needs to be in DEBUG, INFO, WARNING, ERROR or CRITICAL ")
+                print_usage()
+
 
 if __name__ == "__main__":
     main()
