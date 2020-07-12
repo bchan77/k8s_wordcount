cd19dc133c03d6bb60ac91b1835fc1af132dc5d6
Billy <billy@datarobot.com>
2020-07-11 21:39:03 -0400

    Working on getting PVC

--- src/k8s_wordcount.py
+++ src/k8s_wordcount.py
@@ -6,6 +6,17 @@ def get_namespace():
     #Get a list of namespace
     print("get_namespace")
 
+def get_all_pvc(kube_client, namespace):
+    #Return a list of pvc
+    logging.debug("Getting A list of PVC")
+    logging.debug("=====================")
+    pvcs = []
+
+
+    for i in kube_client.list_namespaced_persistent_volume_claim(namespace, watch=False):
+        logging.debug(str(i.metadata.name))
+        pvcs.append(str(i.metadata.name))
+
 
 def create_pvc_if_not_exist():
     print("create_pvc_if_not_exist")
@@ -19,24 +30,30 @@ def print_usage():
 
 def main():
     LOGGING=logging.INFO # By default, just log info
+    NAMESPACE=""
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "", ["--log="])
+        opts, args = getopt.getopt(sys.argv[1:], "-h", ["log="])
 
     except getopt.GetoptError:
+        print("hello world2")
         print_usage()
         sys.exit(-1)
 
     for opt,arg in opts:
         if opt.upper() == ("--LOG"):
             if(arg.upper() not in ['DEBUG', "INFO", "WARNING", "ERROR","CRITICAL"]):
-                print("ERROR: --Log needs to be in DEBUG, INFO, WARNING, ERROR or CRITICAL ")
-                print_usage()
+                print("ERROR: --log needs to be in DEBUG, INFO, WARNING, ERROR or CRITICAL ")
+        else:
+            print_usage()
 
     #Lets get the KUBECONFIG
     config.load_kube_config()
     kube_client = client.CoreV1Api()
-    
+
+    #Testing only
+    get_all_pvc(kube_client,NAMESPACE)
+
 
 
 if __name__ == "__main__":
