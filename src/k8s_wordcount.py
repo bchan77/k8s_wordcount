d8db975527f1ab4c056a3678cc8bfe219f0c579d
Billy <billy@datarobot.com>
2020-07-12 18:24:32 -0400

    get_all_pvc is working. Added logger

--- src/k8s_wordcount.py
+++ src/k8s_wordcount.py
@@ -10,20 +10,21 @@ def get_all_pvc(kube_client, namespace):
     #Return a list of pvc
     logging.debug("Getting A list of PVC")
     logging.debug("=====================")
-    pvcs = []
+    pvc_list = []
 
+    pvcs = kube_client.list_namespaced_persistent_volume_claim(namespace, watch=False)
 
-    for i in kube_client.list_namespaced_persistent_volume_claim(namespace, watch=False):
-        logging.debug(str(i.metadata.name))
-        pvcs.append(str(i.metadata.name))
+    for i in pvcs.items:
+        pvc_list.append(str(i.metadata.name))
 
+    return pvc_list
 
 def create_pvc_if_not_exist():
     print("create_pvc_if_not_exist")
 
 
 def print_usage():
-    print(sys.argv[0] +  "-h " \
+    print(sys.argv[0] +  " -h " \
                          "--LOG <DEBUG|INFO|WARNING|ERROR|CRITICAL>"
           )
 
@@ -35,24 +36,37 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "-h", ["log="])
 
-    except getopt.GetoptError:
-        print("hello world2")
+    except getopt.GetoptError as err:
+        print(str(err))
         print_usage()
         sys.exit(-1)
 
     for opt,arg in opts:
-        if opt.upper() == ("--LOG"):
-            if(arg.upper() not in ['DEBUG', "INFO", "WARNING", "ERROR","CRITICAL"]):
+        if opt.upper() == "--LOG":
+            if arg.upper() == "DEBUG":
+                logging.basicConfig(level=logging.DEBUG)
+            elif arg.upper() == "INFO":
+                logging.basicConfig(level=logging.INFO)
+            elif arg.upper() == "WARNING":
+                logging.basicConfig(level=logging.WARNING)
+            elif arg.upper() == "ERROR":
+                logging.basicConfig(level=logging.ERROR)
+            elif arg.upper() == "CRITICAL":
+                logging.basicConfig(level=logging.CRITICAL)
+            else:
                 print("ERROR: --log needs to be in DEBUG, INFO, WARNING, ERROR or CRITICAL ")
-        else:
+        elif opt == "-h":
             print_usage()
+
 
     #Lets get the KUBECONFIG
     config.load_kube_config()
     kube_client = client.CoreV1Api()
 
     #Testing only
-    get_all_pvc(kube_client,NAMESPACE)
+    pvcs = get_all_pvc(kube_client,NAMESPACE)
+
+    print(pvcs)
 
 
 
